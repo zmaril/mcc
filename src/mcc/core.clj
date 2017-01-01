ed3e4e78eba9bd3a07e717e1a896bf36d2180827
Zack Maril <zack@zacharymaril.com>
2017-01-01 17:18:59 -0500

    Cleaing up core

--- src/mcc/core.clj
+++ src/mcc/core.clj
@@ -12,35 +12,35 @@
    [com.rpl.specter :refer :all]
    [datascript.core :as d]))
 
-(def testtest (slurp  "dev-resources/sample.c"))
+(defn string->db [s]
+  (let [chunked (-> s into-bundles into-chunks)]))
+(def sample (slurp  "dev-resources/sample.c"))
 
-(def bundled (into-bundles testtest))
-(def tags (map :type bundled))
-(def test-chunks (into-chunks bundled))
-(map join (cart (map produce-text test-chunks)))
-
+(def bundled (into-bundles sample))
+(def chunked (into-chunks bundled))
+(map join (cart (map produce-text chunked)))
 
 (s/explain :mcc/bundles bundled)
 (s/conform :mcc/bundles bundled)
 
 (s/explain (s/* :mcc/chunked) bundled)
 (s/conform (s/* :mcc/chunked) bundled)
-(apply diff (map clean-parse (produce-strings test-chunks)))
 
-(def testparse
- (clean-parse (first (produce-strings test-chunks))))
-(def metad (enlive-output->datascript-datums testparse))
+(apply diff (map clean-parse (produce-strings chunked)))
+
+(def parsed
+ (clean-parse (first (produce-strings chunked))))
+
+(def datums-sample (enlive-output->datascript-datums parsed))
 (def conn (d/create-conn schema))
-(d/transact! conn [metad])
-
-(def printf
- (d/q '[:find  ?prize
-        :where [?value :value "printf"]
-               [?symbol :content ?value]
-               [?fun-call :content ?symbol]
-               [?fun-call :content ?args]
-               [?args :order 1]
-               [?args :content ?temp1]
-               [?temp1 :content ?temp2]
-               [?temp2 :value ?prize]]
-      @conn))
+(d/transact! conn [datums-sample])
+(d/q '[:find  ?prize
+       :where [?value :value "printf"]
+              [?symbol :content ?value]
+              [?fun-call :content ?symbol]
+              [?fun-call :content ?args]
+              [?args :order 1]
+              [?args :content ?temp1]
+              [?temp1 :content ?temp2]
+              [?temp2 :value ?prize]]
+     @conn)
